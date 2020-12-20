8f96d9d3a5637cc7e63fba9cac776e1957fc5290
Makentoshe <mkliverout@gmail.com>
2020-12-20 22:42:11 +0300

    feat: Add creating custom file in project

--- src/main/kotlin/com/makentoshe/sipichan/plugin/wizard/SpaceModuleBuilder.kt
+++ src/main/kotlin/com/makentoshe/sipichan/plugin/wizard/SpaceModuleBuilder.kt
@@ -5,14 +5,20 @@ import com.intellij.ide.util.projectWizard.ModuleWizardStep
 import com.intellij.ide.util.projectWizard.WizardContext
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.module.ModuleType
+import com.intellij.openapi.options.ConfigurationException
 import com.intellij.openapi.roots.ModifiableRootModel
 import com.intellij.openapi.roots.ui.configuration.ModulesProvider
 import com.intellij.openapi.util.io.FileUtilRt
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.openapi.vfs.VirtualFile
 import com.makentoshe.sipichan.plugin.wizard.step.FirstModuleWizardStep
 import com.makentoshe.sipichan.plugin.wizard.step.SecondModuleWizardStep
 import java.io.File
+import java.io.IOException
+import java.nio.file.Files
+
 
 class SpaceModuleBuilder : ModuleBuilder() {
 
@@ -29,14 +35,15 @@ class SpaceModuleBuilder : ModuleBuilder() {
         val contentRootDirectory = File(contentEntryPath)
         FileUtilRt.createDirectory(contentRootDirectory)
         val localFileSystem = LocalFileSystem.getInstance()
-        val modelContentRootDirectory = localFileSystem.refreshAndFindFileByIoFile(contentRootDirectory) ?: return
-        modifiableRootModel.addContentEntry(modelContentRootDirectory)
+        val virtualRootDirectory = localFileSystem.refreshAndFindFileByIoFile(contentRootDirectory) ?: return
+        modifiableRootModel.addContentEntry(virtualRootDirectory)
 
         // receive project and module
         val project = modifiableRootModel.project
         val module = modifiableRootModel.module
         println(project)
         println(module)
+        setupTestFile(virtualRootDirectory)
     }
 
     override fun createWizardSteps(
@@ -48,6 +55,35 @@ class SpaceModuleBuilder : ModuleBuilder() {
 
     override fun getCustomOptionsStep(context: WizardContext, parentDisposable: Disposable): ModuleWizardStep {
         return InitialSpaceModuleWizardStep(context, parentDisposable)
+    }
+
+    private fun setupTestFile(projectRoot: VirtualFile) {
+        val title = "build.gradle"
+        val file = try {
+            createVirtualFile(projectRoot, "gradle.build.sas")
+        } catch (exception: IOException) {
+            throw ConfigurationException(exception.message)
+        }
+
+        println(file)
+    }
+
+    private fun createVirtualFile(parent: VirtualFile, title: String): VirtualFile {
+        val file = parent.toNioPath().resolve(title)
+
+        Files.createDirectories(file.parent)
+        Files.createFile(file)
+
+        val virtualFile = VfsUtil.findFile(file, true)
+        // Todo specify message
+            ?: throw ConfigurationException("VirtualFile($parent/$title) is null")
+        if (virtualFile.isDirectory) {
+            // Todo specify message
+            throw ConfigurationException("VirtualFile($parent/$title) is directory")
+        }
+
+        VfsUtil.markDirtyAndRefresh(false, false, false, virtualFile)
+        return virtualFile
     }
 
 }
