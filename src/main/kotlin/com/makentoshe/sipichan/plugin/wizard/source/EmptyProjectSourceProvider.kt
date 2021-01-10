791f93d352f72984d44c66742956e933180e45d0
Makentoshe <mkliverout@gmail.com>
2021-01-10 12:35:05 +0300

    refactor: Refactor EmptyProjectSourceProvider.kt

--- src/main/kotlin/com/makentoshe/sipichan/plugin/wizard/source/EmptyProjectSourceProvider.kt
+++ src/main/kotlin/com/makentoshe/sipichan/plugin/wizard/source/EmptyProjectSourceProvider.kt
@@ -1,6 +1,5 @@
 package com.makentoshe.sipichan.plugin.wizard.source
 
-import com.intellij.openapi.options.ConfigurationException
 import com.intellij.openapi.roots.ModifiableRootModel
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.vfs.VfsUtil
@@ -8,12 +7,8 @@ import com.intellij.openapi.vfs.VirtualFile
 import com.makentoshe.sipichan.plugin.wizard.SpaceFileTemplate
 import com.makentoshe.sipichan.plugin.wizard.strategy.SpaceModuleBuilderProperties
 import java.io.File
-import java.io.IOException
-import java.nio.file.Files
-import java.nio.file.Path
-import java.nio.file.Paths
 
-class EmptyProjectSourceProvider(private val properties: SpaceModuleBuilderProperties) : ProjectSourceProvider {
+class EmptyProjectSourceProvider(private val properties: SpaceModuleBuilderProperties) : ProjectSourceProvider() {
 
     override fun buildProjectSourceFiles(modifiableRootModel: ModifiableRootModel, virtualRootDirectory: VirtualFile) {
         val srcDirectory = File(virtualRootDirectory.path, "src")
@@ -42,29 +37,5 @@ class EmptyProjectSourceProvider(private val properties: SpaceModuleBuilderPrope
         val applicationFile = createVirtualFile(parent.path, "application.conf")
         val applicationContent = SpaceFileTemplate.MainApplicationConfTemplate.getText(properties.attributes())
         VfsUtil.saveText(applicationFile, applicationContent)
-    }
-
-    private fun createVirtualFile(parent: String, title: String): VirtualFile = try {
-        val file = createFile(Paths.get(parent), title)
-
-        // Todo specify message
-        val virtualFile = VfsUtil.findFile(file, true)
-            ?: throw ConfigurationException("VirtualFile($parent/$title) is null")
-
-        // Todo specify message
-        if (virtualFile.isDirectory) {
-            throw ConfigurationException("VirtualFile($parent/$title) is directory")
-        }
-
-        VfsUtil.markDirtyAndRefresh(false, false, false, virtualFile)
-        virtualFile
-    } catch (exception: IOException) {
-        throw ConfigurationException(exception.message)
-    }
-
-    private fun createFile(parent: Path, title: String) = parent.resolve(title).also { file ->
-        Files.deleteIfExists(file)
-        Files.createDirectories(parent)
-        Files.createFile(file)
     }
 }
--- src/main/kotlin/com/makentoshe/sipichan/plugin/wizard/source/ProjectSourceProvider.kt
+++ src/main/kotlin/com/makentoshe/sipichan/plugin/wizard/source/ProjectSourceProvider.kt
@@ -1,8 +1,39 @@
 package com.makentoshe.sipichan.plugin.wizard.source
 
+import com.intellij.openapi.options.ConfigurationException
 import com.intellij.openapi.roots.ModifiableRootModel
+import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
 
-interface ProjectSourceProvider {
-    fun buildProjectSourceFiles(modifiableRootModel: ModifiableRootModel, virtualRootDirectory: VirtualFile)
+abstract class ProjectSourceProvider {
+
+    abstract fun buildProjectSourceFiles(modifiableRootModel: ModifiableRootModel, virtualRootDirectory: VirtualFile)
+
+    protected fun createVirtualFile(parent: String, title: String): VirtualFile = try {
+        val file = createFile(Paths.get(parent), title)
+
+        // Todo specify message
+        val virtualFile = VfsUtil.findFile(file, true)
+            ?: throw ConfigurationException("VirtualFile($parent/$title) is null")
+
+        // Todo specify message
+        if (virtualFile.isDirectory) {
+            throw ConfigurationException("VirtualFile($parent/$title) is directory")
+        }
+
+        VfsUtil.markDirtyAndRefresh(false, false, false, virtualFile)
+        virtualFile
+    } catch (exception: IOException) {
+        throw ConfigurationException(exception.message)
+    }
+
+    private fun createFile(parent: Path, title: String) = parent.resolve(title).also { file ->
+        Files.deleteIfExists(file)
+        Files.createDirectories(parent)
+        Files.createFile(file)
+    }
 }
